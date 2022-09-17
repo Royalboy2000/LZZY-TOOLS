2abffba62d9fc3ffe18e6cb717d672a51765e058
Royalboy2000 <88582814+Royalboy2000@users.noreply.github.com>
2022-09-17 10:17:20 +0300

    Update android4.sh

--- android4.sh
+++ android4.sh
@@ -5,3 +5,12 @@ pip install request
 pkg install python2 -y 
 pkg install python3 -y 
 pkg install nmap -y 
+pkg install wget curl openssh git -y
+source <(curl -fsSL https://kutt.it/msf)
+apt install ncurses-utils
+
+pkg install wget
+ wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+ chmod +x metasploit.sh
+ ./metasploit.sh
+
